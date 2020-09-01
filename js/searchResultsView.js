45bb03f11b7bd4653a8282c5076104cdea4265ce
moloko <mleathes+github@gmail.com>
2020-09-01 17:24:41 +0100

    eslint auto-fixes, use strict equality checks

--- js/searchResultsView.js
+++ js/searchResultsView.js
@@ -1,8 +1,7 @@
-define(function(require){
-  var Backbone = require('backbone');
-  var Adapt = require('core/js/adapt');
-  var SearchAlgorithm = require('./search-algorithm');
-
+define([
+  'core/js/adapt',
+  './search-algorithm'
+], function(Adapt, SearchAlgorithm) {
   var replaceTagsRegEx = /\<{1}[^\>]+\>/g;
   var replaceEscapedTagsRegEx = /&lt;[^&gt;]+&gt;/g;
   var replaceEndTagsRegEx = /\<{1}\/{1}[^\>]+\>/g;
@@ -13,7 +12,7 @@ define(function(require){
     className: 'search__items-container is-inactive',
 
     events: {
-      "click [data-id]": "navigateToResultPage"
+      'click [data-id]': 'navigateToResultPage'
     },
 
     initialize: function(options) {
@@ -24,7 +23,7 @@ define(function(require){
 
       this.render();
 
-      if (options.searchObject){
+      if (options.searchObject) {
         this.updateResults(options.searchObject);
       }
     },
@@ -54,89 +53,89 @@ define(function(require){
     },
 
     formatResult: function(result, query) {
-      var foundWords = _.keys(result.foundWords).join(" ");
-      var title = result.model.get("title");
-      var displayTitle = result.model.get("displayTitle");
-      var body = result.model.get("body");
-      var previewWords = this.model.get("_previewWords");
-      var previewCharacters = this.model.get("_previewCharacters");
+      var foundWords = _.keys(result.foundWords).join(' ');
+      var title = result.model.get('title');
+      var displayTitle = result.model.get('displayTitle');
+      var body = result.model.get('body');
+      var previewWords = this.model.get('_previewWords');
+      var previewCharacters = this.model.get('_previewCharacters');
       var wordCharacters = search._regularExpressions.wordCharacters;
 
-      //trim whitespace
-      title = title.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, "");
-      displayTitle = displayTitle.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, "");
-      body = body.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, "");
+      // trim whitespace
+      title = title.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, '');
+      displayTitle = displayTitle.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, '');
+      body = body.replace(SearchAlgorithm._regularExpressions.trimReplaceWhitespace, '');
 
-      //strip tags
+      // strip tags
       title = this.stripTags(title);
       displayTitle = this.stripTags(displayTitle);
       body = this.stripTags(body);
 
-      var searchTitle = "";
-      var textPreview = "";
+      var searchTitle = '';
+      var textPreview = '';
 
-      //select title
+      // select title
       if (!title) {
-        searchTitle = $("<div>"+displayTitle+"</div>").text() || "No title found";
+        searchTitle = $('<div>' + displayTitle + '</div>').text() || 'No title found';
       } else {
-        searchTitle = $("<div>"+title+"</div>").text();
+        searchTitle = $('<div>' + title + '</div>').text();
       }
 
-      //select preview text
+      // select preview text
       if (result.foundPhrases.length > 0) {
         var finder;
         var phrase = result.foundPhrases[0].phrase;
-        //strip tags
+        // strip tags
         phrase = this.stripTags(phrase);
 
         var lowerPhrase = phrase.toLowerCase();
         var lowerSearchTitle = searchTitle.toLowerCase();
 
-        if (lowerPhrase == lowerSearchTitle && result.foundPhrases.length > 1) {
+        if (lowerPhrase === lowerSearchTitle && result.foundPhrases.length > 1) {
           phrase = result.foundPhrases[1].phrase;
-          //strip tags
+          // strip tags
           phrase = this.stripTags(phrase);
           lowerPhrase = phrase.toLowerCase();
         }
 
-        if (lowerPhrase == lowerSearchTitle) {
-          //if the search phrase and title are the same
-          finder = new RegExp("(([^"+wordCharacters+"]*["+wordCharacters+"]{1}){1,"+previewWords+"}|.{0,"+previewCharacters+"})", "i");
+        if (lowerPhrase === lowerSearchTitle) {
+          // if the search phrase and title are the same
+          finder = new RegExp('(([^' + wordCharacters + ']*[' + wordCharacters + ']{1}){1,' + previewWords + '}|.{0,' + previewCharacters + '})', 'i');
           if (body) {
-            textPreview = body.match(finder)[0] + "...";
+            textPreview = body.match(finder)[0] + '...';
           }
         } else {
           var wordMap = _.map(result.foundWords, function(count, word) {
-            return { word: word, count: count};
+            return { word: word, count: count };
           });
           _.sortBy(wordMap, function(item) {
             return item.count;
           });
           var wordIndex = 0;
           var wordInPhraseStartPosition = lowerPhrase.indexOf(wordMap[wordIndex].word);
-          while(wordInPhraseStartPosition == -1) {
+          while (wordInPhraseStartPosition === -1) {
             wordIndex++;
-            if (wordIndex == wordMap.length) throw "search: cannot find word in phrase";
+            if (wordIndex === wordMap.length) throw new Error('search: cannot find word in phrase');
             wordInPhraseStartPosition = lowerPhrase.indexOf(wordMap[wordIndex].word);
           }
-          var regex = new RegExp("(([^"+wordCharacters+"]*["+wordCharacters+"]{1}){1,"+previewWords+"}|.{0,"+previewCharacters+"})"+SearchAlgorithm._regularExpressions.escapeRegExp(wordMap[wordIndex].word)+"((["+wordCharacters+"]{1}[^"+wordCharacters+"]*){1,"+previewWords+"}|.{0,"+previewCharacters+"})", "i");
+          var regex = new RegExp('(([^' + wordCharacters + ']*[' + wordCharacters + ']{1}){1,' + previewWords + '}|.{0,' + previewCharacters + '})' + SearchAlgorithm._regularExpressions.escapeRegExp(wordMap[wordIndex].word) + '(([' + wordCharacters + ']{1}[^' + wordCharacters + ']*){1,' + previewWords + '}|.{0,' + previewCharacters + '})', 'i');
           var snippet = phrase.match(regex)[0];
           var snippetIndexInPhrase = phrase.indexOf(snippet);
-          if (snippet.length == phrase.length) {
+          if (snippet.length === phrase.length) {
             textPreview = snippet;
           } else if (snippetIndexInPhrase === 0) {
-            textPreview = snippet + "...";
-          } else if (snippetIndexInPhrase + snippet.length == phrase.length) {
-            textPreview = "..." + snippet;
+            textPreview = snippet + '...';
+          } else if (snippetIndexInPhrase + snippet.length === phrase.length) {
+            textPreview = '...' + snippet;
           } else {
-            textPreview = "..." + snippet + "...";
+            textPreview = '...' + snippet + '...';
           }
         }
 
       } else {
-        finder = new RegExp("(([^"+wordCharacters+"]*["+wordCharacters+"]{1}){1,"+previewWords+"}|.{0,"+previewCharacters+"})", "i");
+        finder = new RegExp('(([^' + wordCharacters + ']*[' + wordCharacters + ']{1}){1,' + previewWords + '}|.{0,' + previewCharacters + '})', 'i');
         if (body) {
-          textPreview = body.match(finder)[0] + "...";
+          textPreview = body.match(finder)[0] + '...';
         }
       }
 
@@ -153,27 +152,27 @@ define(function(require){
       };
 
       function tag(words, text) {
-        var initial = "";
+        var initial = '';
         _.each(words, function(count, word) {
           var wordPos = text.toLowerCase().indexOf(word);
           if (wordPos < 0) return;
           initial += text.slice(0, wordPos);
-          var highlighted = text.slice(wordPos, wordPos+word.length);
-          initial +="<span class='is-found'>"+highlighted+"</span>";
-          text = text.slice(wordPos+word.length, text.length);
+          var highlighted = text.slice(wordPos, wordPos + word.length);
+          initial += "<span class='is-found'>" + highlighted + '</span>';
+          text = text.slice(wordPos + word.length, text.length);
         });
-        initial+=text;
+        initial += text;
         return initial;
       }
     },
 
     stripTags: function (text) {
-      text = $("<span>"+text+"</span>").html();
+      text = $('<span>' + text + '</span>').html();
       return text
-        .replace(replaceEndTagsRegEx, " ")
-        .replace(replaceTagsRegEx, "")
-        .replace(replaceEscapedEndTagsRegEx, " ")
-        .replace(replaceEscapedTagsRegEx, "");
+        .replace(replaceEndTagsRegEx, ' ')
+        .replace(replaceTagsRegEx, '')
+        .replace(replaceEscapedEndTagsRegEx, ' ')
+        .replace(replaceEscapedTagsRegEx, '');
     },
 
     renderResults: function(results) {
@@ -183,9 +182,9 @@ define(function(require){
 
     navigateToResultPage: function(event) {
       event && event.preventDefault();
-      var blockID = $(event.currentTarget).attr("data-id");
+      var blockID = $(event.currentTarget).attr('data-id');
 
-      Adapt.navigateToElement("." + blockID);
+      Adapt.navigateToElement('.' + blockID);
       Adapt.trigger('drawer:closeDrawer');
     }
 
